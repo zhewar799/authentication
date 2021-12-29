117f1d994b5e08053f38d0cb2bdd5fed0698e154
zhewar omer <zhewaromer2@gmail.com>
2021-12-29 20:43:50 +0300

    level 3

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -21,7 +19,6 @@ const userSchema = new mongoose.Schema( {
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,7 +37,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password:req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if (err) {
@@ -53,9 +50,9 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
-    User.findOne({email: username}, function(req, foundUser){
+    User.findOne({email: username}, function(err, foundUser){
         if (err){
             console.log(err);
         }else {
